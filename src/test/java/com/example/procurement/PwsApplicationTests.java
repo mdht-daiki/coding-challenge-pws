b9fb79af55f6517a0176a771b761b51500eb9bf2
mdht-daiki <70727838+mdht-daiki@users.noreply.github.com>
2025-10-09 07:57:17 +0900

    CIテスト用の設定を追加 (#1)
    * CIテスト用の設定を追加
    
    * JPA無効化設定を変更
    
    * MockBeanをMockitoBeanに変更
    
    * codeql設定を追加
    
    * CodeQL設定ファイルの位置を修正
    
    * ワイルドカードを文字列として読めるように修正

--- src/test/java/com/example/procurement/PwsApplicationTests.java
+++ src/test/java/com/example/procurement/PwsApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.procurement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("slice-test")
 class PwsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
--- src/test/java/com/example/procurement/web/RequestControllerTest.java
+++ src/test/java/com/example/procurement/web/RequestControllerTest.java
@@ -7,8 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
@@ -17,13 +18,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("slice-test")
 @WebMvcTest(RequestController.class)
 public class RequestControllerTest {
 
     @Autowired
     MockMvc mvc;
 
-    @MockBean
+    @MockitoBean
     RequestService service;
 
     @Test
