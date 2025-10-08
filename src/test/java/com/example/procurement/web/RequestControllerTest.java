103a392ec781c0f35afa72639f4f20ebb7eaba45
dkmdhtWin <dkyamagiwa156@gmail.com>
2025-10-09 06:54:22 +0900

    MockBeanをMockitoBeanに変更

--- src/test/java/com/example/procurement/web/RequestControllerTest.java
+++ src/test/java/com/example/procurement/web/RequestControllerTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
@@ -25,7 +25,7 @@ public class RequestControllerTest {
     @Autowired
     MockMvc mvc;
 
-    @MockBean
+    @MockitoBean
     RequestService service;
 
     @Test
