3f0ad1b23c888210119952d44fff4578f2633030
dkmdhtWin <dkyamagiwa156@gmail.com>
2025-10-08 23:21:58 +0900

    CIテスト用の設定を追加

--- src/test/java/com/example/procurement/web/RequestControllerTest.java
+++ src/test/java/com/example/procurement/web/RequestControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("test")
 @WebMvcTest(RequestController.class)
 public class RequestControllerTest {
 
