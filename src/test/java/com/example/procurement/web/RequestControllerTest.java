04306913f7145853885621a173a3c3d0b5f151be
dkmdhtWin <dkyamagiwa156@gmail.com>
2025-10-09 06:43:38 +0900

    JPA無効化設定を変更

--- src/test/java/com/example/procurement/web/RequestControllerTest.java
+++ src/test/java/com/example/procurement/web/RequestControllerTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ActiveProfiles("test")
+@ActiveProfiles("slice-test")
 @WebMvcTest(RequestController.class)
 public class RequestControllerTest {
 
